c9f8bdeea82f379e6a4b6f41207d36e59995cc17
Harshithhk <dota2kelkar@gmail.com>
2025-09-13 18:20:32 -0400

    completed daily_temperatures from stack

--- stack/daily_temperatures.py
+++ stack/daily_temperatures.py
@@ -1,14 +1,12 @@
-def dailyTemperatures(self, temperatures):
-    res = []
-    left = 0
-    right = 1
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        res = [0] * len(temperatures)
+        mono_stack = []
+        for i in range(len(temperatures)):
+            while mono_stack and mono_stack[-1][1] < temperatures[i]:
+                index, _ = mono_stack.pop()
+                res[index] = i - index
+            mono_stack.append((i, temperatures[i]))
+            print(mono_stack)
 
-    for i in range(len(temperatures) - 1):
-        diff = temperatures[right] - temperatures[left]
-        
-        if diff > 0:
-            res.append(diff)
-        
-        for j in range (len(temperatures) - i):
-            diff = temperatures[right] - temperatures[left]
-            
+        return res
