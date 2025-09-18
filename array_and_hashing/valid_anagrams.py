227dad9f19faa1ad3f6f0d2adaafc2dd3840ac68
Harshithhk <dota2kelkar@gmail.com>
2025-09-18 19:24:12 -0400

    valid anagrams

--- array_and_hashing/valid_anagrams.py
+++ array_and_hashing/valid_anagrams.py
@@ -1,18 +1,15 @@
-class Solution(object):
-    def isAnagram(self, s, t):
-        """
-        :type s: str
-        :type t: str
-        :rtype: bool
-        """
-
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
         if len(s) != len(t):
             return False
 
-        dict1, dict2 = {}, {}
+        counts = {}
+        for ch in s:
+            counts[ch] = counts.get(ch, 0) + 1
 
-        for i in range(len(s)):
-            dict1[s[i]] = 1 + dict1.get(s[i], 0)
-            dict2[t[i]] = 1 + dict2.get(t[i], 0)
+        for ch in t:
+            if ch not in counts or counts[ch] == 0:
+                return False
+            counts[ch] -= 1
 
-        return dict1 == dict2
+        return True
